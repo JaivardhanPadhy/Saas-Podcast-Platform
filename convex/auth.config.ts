13612edb7550d583713770a1edd66cfb9d3afe4a
JaivardhanPadhy <164767602+JaivardhanPadhy@users.noreply.github.com>
2024-07-11 19:21:28 +0530

    Add files via upload

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig={
+    providers: [
+      {
+        domain: "https://relieved-mallard-6.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+  export default authConfig;
